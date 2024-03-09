3189c97021d5c64b9b1627b4c83d8a6d339e260b
Zayyan <qureshizayyan49@gmail.com>
2024-03-10 04:34:05 +0530

    make some changes

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// window.location.="http://www.sourceofsupplements.com";
-
-document.querySelector('.img').addEventListener('click', function () {
-    window.location;
-})
+// document.body.style.overflow = 'hidden';
