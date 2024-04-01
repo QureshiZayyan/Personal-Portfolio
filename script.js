5abcd7fc03f92c0bfe8d0f82a8b03b86a6600b5c
Zayyan <qureshizayyan49@gmail.com>
2024-04-01 07:11:28 +0530

    make some changes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var typed = new Typed('#element', {
-    strings: ['Technology Enthusiast', 'Graphics Designer', 'Software Developer'],
+    strings: ['Technology Enthusiast', 'Programmer', 'Software Developer'],
     typeSpeed: 55,
-    loop: true,
+    // loop: true,
 });
+
+document.getElementById('linkedin').addEventListener('click', () => window.location.href = 'https://www.linkedin.com/in/zayyanqureshi/');
+document.getElementById('github').addEventListener('click', () => window.location.href = 'https://github.com/QureshiZayyan');
+document.getElementById('twitter').addEventListener('click', () => window.location.href = 'https://twitter.com/qureshizayyan1');
+document.getElementById('instagram').addEventListener('click', () => window.location.href = 'https://www.instagram.com/qureshi_zayyan/');
