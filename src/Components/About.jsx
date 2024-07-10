a11614e54174dffc3e1c383b13df96a53a3b74f6
Zayyan <qureshizayyan49@gmail.com>
2024-07-11 04:22:34 +0530

    done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,9 +3,9 @@ import emailIcon from '../assets/email.png';
 export const About = () => {
     return (
         <>
-            <section className="about shadow-bs leading-6 text-white flex items-center lg:flex-row md:justify-center md:flex-col text-[1.1rem] pt-[32px] lg:pr-[9px] lg:pb-[23px] lg:pl-[80px] my-40 md:pl-[10px] md:pr-[40px] md:pb-[17px]">
+            <section className="about shadow-bs leading-6 text-white flex items-center lg:flex-row md:justify-center md:flex-col text-base pt-[32px] lg:pr-[9px] lg:pb-[23px] lg:pl-[80px] my-[110px] md:pl-[10px] md:pr-[40px] md:pb-[17px]">
                 <div className="aboutMe lg:w-[35vw] md:w-[85vw]">
-                    <h3 className="font-bold text-white text-[1.5rem] mb-[10px]">About Me</h3>
+                    <h3 className="font-bold text-white mb-[10px] text-2xl">About Me</h3>
 
                     <hr className="text-purpleclr" />
                     <p className="my-[17px]">I am extremely proficient in Front-End Development using <span className="skills font-semibold">HTML5, Cascading
@@ -17,8 +17,8 @@ export const About = () => {
                     <p className="my-[17px]">Graduated with a degree in <strong>(Bachelor of Science in Information Technology)</strong>.</p>
                 </div>
 
-                <div className="query ml-[150px] mt-[26px] text-center md:mx-auto">
-                    <p className="font-bold text-[19px]">Any type of query & Discussion</p>
+                <div className="query lg:ml-[150px] mt-[26px] text-center md:mx-auto">
+                    <p className="font-bold">Any type of query & Discussion</p>
                     <hr className="mt-[14px] mb-[8px]" />
                     <span className="flex items-center justify-center mt-[6px] mx-auto">
                         <img src={emailIcon} alt="'email" className="w-[30px] mr-[7px]" />
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,10 +6,10 @@ export const Cards = (props) => {
             <div className="card w-[15vw] cursor-pointer hover:translate-y-[-2px] hover:opacity-[0.6] duration-300">
                 <a href={props.link} target='_blank'>
                     <div className="image">
-                        <img className="w-full xl:h-[12vh] lg:h-[15.8vh] md:h-[184px] rounded-t-[8.5px]" src={props.imageurl} alt={props.name} />
+                        <img className="w-full xl:h-[12vh] lg:h-[17vh] md:h-[184px] rounded-t-[8.5px]" src={props.imageurl} alt={props.name} />
                     </div>
-                    <div className="project-content xl:h-[10vh] mt-[-0.5px] px-[5px] py-[5px] rounded-b-[8.5px] bg-white">
-                        <h3 className="font-bold text-base leading-6 mb-[5px]">{props.name}</h3>
+                    <div className="project-content text-base xl:h-[9vh] lg:h-[15vh] mt-[-0.5px] px-[5px] py-[5px] rounded-b-[8.5px] bg-white">
+                        <h3 className="font-bold leading-6 mb-[5px]">{props.name}</h3>
                         <p className="about-project mt-[1px] mb-[5px]">{props.about}</p>
                     </div>
                 </a>
