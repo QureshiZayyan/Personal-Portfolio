ac2ae369f4bb91baa8150a1662d7b17c2b479830
Zayyan <qureshizayyan49@gmail.com>
2024-08-13 00:42:22 +0530

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ export const App = () => {
         <About />
         <section className="project mt-0 mx-[40px] mb-[150px]">
           <h2 className="project-heading w-[10%] font-bold text-[2.1rem] text-white border-b-[1.5px] mb-[20px]">Projects</h2>
-          <div className="cards-container grid lg:grid-cols-5 md:grid-cols-2 my-[50px] place-items-center md:gap-y-12 md:mx-[18vw] xl:mx-24 lg:mx-[0]">
-            <Cards name="News Search App" imageurl={image2} link='https://github.com/QureshiZayyan/News-App-React' about='React,Tailwind' />
-            <Cards name="Live Weather Search" imageurl={image3} link='https://searchweathers.vercel.app/' />
-            <Cards name="Recipe Finder App" imageurl={recipe} link='https://search-recipess.vercel.app/' about='React' />
+          <div className="cards-container grid lg:grid-cols-5 md:grid-cols-2 my-[50px] place-items-center md:gap-y-11 md:mx-[19vw] xl:mx-24 lg:mx-[8vw]">
+            <Cards name="News App" imageurl={image2} link='https://github.com/QureshiZayyan/News-App-React' about='React,Tailwind' />
+            <Cards name="Live Weather App" imageurl={image3} link='https://searchweathers.vercel.app/' />
+            <Cards name="Recipe App" imageurl={recipe} link='https://search-recipess.vercel.app/' about='React' />
             <Cards name="ToDo List App" imageurl={image1} link='https://qureshizayyan.github.io/ToDo-List/' about='HTML,CSS,JS' />
-            <Cards name="Microsoft UI Clone" imageurl={mslogo} link='https://microsoftuiclone.vercel.app/' about='React,Tailwind' />
+            <Cards name="Microsoft UI" imageurl={mslogo} link='https://microsoftuiclone.vercel.app/' about='React,Tailwind' />
           </div>
         </section>
       </main >
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 export const Cards = (props) => {
     return (
         <>
-            <div className="card md:w-[22vw] xl:w-[13.5vw] lg:w-[15.5vw] cursor-pointer hover:translate-y-[-2px] hover:opacity-[0.7] duration-300">
+            <div className="card md:w-[21vw] xl:w-[13.5vw] lg:w-[13vw] cursor-pointer hover:translate-y-[-2px] hover:opacity-[0.7] duration-300">
                 <a href={props.link} target='_blank'>
                     <div className="image">
-                        <img className="w-full xl:h-[90px] lg:h-[84px] md:h-[100px]" src={props.imageurl} alt={props.name} />
+                        <img className="w-full xl:h-[90px] lg:h-[75px] md:h-[85px]" src={props.imageurl} alt={props.name} />
                     </div>
-                    <div className="project-content text-base xl:h-[63px] lg:h-[63px] md:h-[62px] mt-[-0.5px] px-[6px] pt-1 pb-4 bg-white">
-                        <h3 className="font-bold leading-6 mb-[2px]">{props.name}</h3>
+                    <div className="project-content text-base xl:h-[63px] lg:h-[62px] md:h-[62px] mt-[-0.5px] px-[6px] pt-1 pb-4 bg-white">
+                        <h3 className="font-semibold leading-6 mb-[2px]">{props.name}</h3>
                         <p className="about-project mt-[1px] mb-[2px]">{props.about}</p>
                     </div>
                 </a>
