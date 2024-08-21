e88f3f16b65d8c2c9bc3344032399c83a3c506d9
Zayyan <qureshizayyan49@gmail.com>
2024-08-22 05:07:16 +0530

    added changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ export default {
     "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      textColor: {
-        // 'purpleclr': 'rgb(179, 46, 179)',
-      },
+      
       backgroundColor: {
         'bcg': 'rgb(25, 25, 25)',
       },
