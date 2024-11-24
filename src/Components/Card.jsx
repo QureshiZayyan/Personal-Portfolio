90c6da13169246fde78f03a8a046008d3ce1b3d0
zayyan <qureshizayyan49@gmail.com>
2024-11-25 01:14:21 +0530

    changed the card ui

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,12 +4,12 @@ export const Cards = (props) => {
     return (
         <>
 
-            <div className="card xl:w-[13vw] lg:w-[13vw] md:w-[24vw]">
+            <div className="card bg-white xl:w-[13vw] lg:w-[15vw] md:w-[24vw]">
                 <a href={props.link} target='_blank'>
-                    <div className="image">
-                        <img className="w-full bg-black aspect-video" src={props.imageurl} alt={props.name} />
+                    <div className="image p-[8px]">
+                        <img className="w-full rounded-[10px] bg-black aspect-video" src={props.imageurl} alt={props.name} />
                     </div>
-                    <div className="project-content xl:h-[60px] lg:h-[56px] md:h-[62px] mt-[-1px] px-[6px] pt-1 pb-4 bg-white">
+                    <div className="project-content xl:h-[60px] lg:h-[59px] md:h-[62px] mt-[-1px] px-[8px] pt-1 pb-4">
                         <h3 className="font-semibold leading-6 mb-[2px] text-base">{props.name}</h3>
                         <p className="about-project mt-[1px] mb-[2px] text-[14.8px]">{props.about}</p>
                     </div>
