482a46cd223ab1b40ee62fae87f06296618144bb
zayyan <qureshizayyan49@gmail.com>
2024-12-06 05:35:46 +0530

    worked on layout

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -54,7 +54,7 @@ export const Header = () => {
 
       <div className="intro flex items-center mt-[50px] mx-16">
         <div className="left lg:w-[60%] lg:h-auto xl:w-[49%] xl:h-auto md:w-[304px] md:h-[200px]">
-          <h1 className="text-[28px] font-bold text-white leading-[39px] xl:text-[33px]">Hey, My Self <span className="name">Zayyan Qureshi</span>
+          <h1 className="text-[30px] font-bold text-white leading-[39px] xl:text-[33px]">Hey, My Self <span className="name">Zayyan Qureshi</span>
             <br />
             <span>& I&apos;m a Passionate</span>
             <br />
@@ -68,8 +68,8 @@ export const Header = () => {
       </div>
 
       <div className="resume-github mx-[65px] relative lg:-top-8 md:top-3 xl:-top-16">
-        <a href="src/assets/Zayyan Resume.pdf" target="_blank" className="text" download><button className="btn lg:text-[15px] text-[13px] px-[6px] py-[4px] font-[650] cursor-pointer rounded-[5px] bg-white">Download Resume</button></a>
-        <a href="https://github.com/QureshiZayyan" target='_blank' className="text"><button id="btn" className="btn lg:text-[15px] text-[13px] px-[6px] py-[4px] font-[650] cursor-pointer rounded-[5px] bg-white ml-[13px]">Visit Github</button></a>
+        <a href="src/assets/Zayyan Resume.pdf" target="_blank" className="text" download><button className="btn lg:text-[14px] text-[13px] px-[6px] py-[4px] font-[650] cursor-pointer rounded-[5px] bg-white">Download Resume</button></a>
+        <a href="https://github.com/QureshiZayyan" target='_blank' className="text"><button id="btn" className="btn lg:text-[14px] text-[13px] px-[6px] py-[4px] font-[650] cursor-pointer rounded-[5px] bg-white ml-[13px]">Visit Github</button></a>
       </div>
     </>
   )
