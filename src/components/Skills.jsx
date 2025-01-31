1c3d9515372ac519fa2960b5e07380307e46accc
zayyan <qureshizayyan49@gmail.com>
2025-01-31 16:54:48 +0530

    worked on animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,4 @@
-import { easeInOut, motion } from "framer-motion";
+import { easeOut, motion } from "framer-motion";
 import { FaBootstrap } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaReact } from "react-icons/fa";
@@ -7,6 +7,7 @@ import { FaHtml5 } from "react-icons/fa6";
 import { BiLogoCss3 } from "react-icons/bi";
 import { IoLogoJavascript } from "react-icons/io5";
 import { FaNodeJs } from "react-icons/fa";
+import { easeInOut } from "framer-motion/dom";
 
 export const Skills = () => {
     return (
@@ -14,10 +15,13 @@ export const Skills = () => {
 
         >
             <motion.div className="solar-system ml-20"
-                initial={{ opacity: 0, }}
-                whileInView={{ opacity: 1, }}
+                initial={{ opacity: 0, scale: 0.8, y: 50 }}
+                whileInView={{ opacity: 1, scale: 1, y: 0 }}
                 viewport={{ once: true, amount: 0.5 }}
-                transition={{ duration: 0.8, ease: [easeInOut], }}>
+                transition={{
+                    duration: 0.7,
+                    ease: [easeOut],
+                }}>
                 <div className="orbit">
                     <FaBootstrap className="icon" color="#7F18F9" />
                     <RiTailwindCssFill className="icon" color="#3EBFF8" />
@@ -31,10 +35,13 @@ export const Skills = () => {
             </motion.div>
 
             <motion.div id="expertise" className='w-[50%] ml-32'
-                initial={{ opacity: 0, }}
-                whileInView={{ opacity: 1, }}
+                initial={{ opacity: 0, scale: 0.8, y: 50 }}
+                whileInView={{ opacity: 1, scale: 1, y: 0 }}
                 viewport={{ once: true, amount: 0.5 }}
-                transition={{ duration: 0.8, ease: [easeInOut], }}>
+                transition={{
+                    duration: 0.7,
+                    ease: [easeOut],
+                }}>
                 <h3 className="text-3xl mb-3">Skills & Expertise</h3>
                 <p className="before-after text-[26px] leading-9">
                     With a passion for creating responsive and dynamic web applications, I bring expertise in frontend and backend technologies. Whether it's crafting pixel-perfect designs or architecting seamless state management, I thrive on building solutions that make an impact.
